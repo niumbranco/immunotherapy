3899798b64e033818e3f8c32c329e342bfdbfb05
Susana Martínez Arbas <susana.martinez.arbas@gmail.com>
2025-07-10 12:05:35 +0300

    add read files suffix as argument

--- scripts_dev/dada2_filt.R
+++ scripts_dev/dada2_filt.R
@@ -13,8 +13,10 @@ option_list <- list(
   make_option(c("-o", "--output_dir"), type="character", default="dada2_out", help="Output directory [default %default]"),
   make_option(c("--trunc_len_f"), type="integer", default=240, help="Forward read truncation length [default %default]"),
   make_option(c("--trunc_len_r"), type="integer", default=200, help="Reverse read truncation length [default %default]"),
-  make_option(c("-t", "--threads"), type="integer", default=4, help="Number of threads [default %default]")
-)
+  make_option(c("-t", "--threads"), type="integer", default=2, help="Number of threads [default %default]"),
+  make_option(c("-f", "--suffix_1"), type="character", default="_F_filt.fastq.gz", help="Suffix of clean-trimmered forward reads [default %default]"),
+  make_option(c("-r", "--suffix_2"), type="character", default="_R_filt.fastq.gz", help="Suffix of clean-trimmered reverse reads [default %default]")
+  )
 
 # Parse args
 opt <- parse_args(OptionParser(option_list=option_list))
@@ -23,9 +25,9 @@ opt <- parse_args(OptionParser(option_list=option_list))
 dir.create(opt$output_dir, showWarnings = FALSE, recursive = TRUE)
 
 # List input files
-fnFs <- sort(list.files(opt$input_dir, pattern = "_R1_trimmed.fastq.gz$", full.names = TRUE))
-fnRs <- sort(list.files(opt$input_dir, pattern = "_R2_trimmed.fastq.gz$", full.names = TRUE))
-sample.names <- gsub("_R1_trimmed.fastq.gz", "", basename(fnFs))
+fnFs <- sort(list.files(opt$input_dir, pattern = paste0(opt$suffix_1, "$"), full.names = TRUE))
+fnRs <- sort(list.files(opt$input_dir, pattern = paste0(opt$suffix_2, "$"), full.names = TRUE))
+sample.names <- gsub(opt$suffix_1, "", basename(fnFs))
 
 # Define filtered output paths
 filtFs <- file.path(opt$output_dir, paste0(sample.names, "_F_filt.fastq.gz"))
